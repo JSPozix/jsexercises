e4855b75847b462c45baba402226d55b7f13c65e
JSPozix <80532980+JSPozix@users.noreply.github.com>
2021-09-19 09:10:28 +0200

    Add files via upload
    konami exercise

--- app.js
+++ app.js
@@ -1,76 +1,44 @@
-let posts = document.getElementById('dataPosts');
+let dataObj = document.getElementById('list');
 
-let dataObj;
-const data = fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => {
-    return response.json();
-  })
-  .then((json) => {
-    dataObj = json.slice(0, 3);
-    return dataObj;
-  });
+let pre = [];
+const code = 'injects3crets';
+window.addEventListener('keyup', (e) => {
+  console.log(e.keyIdentifier);
 
-const genData = () => {
-  dataObj.map((post) => {
-    const item = document.createElement('li');
-    item.classList.add('list-group-item');
-    const itemTitle = document.createElement('h2');
-    const itemBody = document.createElement('p');
-    let delBtn = document.createElement('button');
-    delBtn.classList.add('btn');
-    delBtn.classList.add('btn-danger');
-    delBtn.innerText = 'x';
-    itemTitle.innerText = post.title;
-    itemBody.innerText = post.body;
-    item.appendChild(itemTitle);
-    item.appendChild(itemBody);
-    item.appendChild(delBtn);
-    posts.appendChild(item);
-  });
-};
-
-const addPost = () => {
-  let pTitle = document.getElementById('postTitle');
-  let pBody = document.getElementById('postBody');
-  let objPost = {
-    id: Math.floor(Math.random() * 100),
-    title: pTitle.value,
-    body: pBody.value,
-  };
-  if (pTitle.value != '' && pBody.value != '') {
-    dataObj.push(objPost);
-    let list = document.createElement('li');
-    list.classList.add('list-group-item');
-    let itemTitle = document.createElement('h2');
-    let itemBody = document.createElement('p');
-    let delBtn = document.createElement('button');
-    delBtn.classList.add('btn');
-    delBtn.classList.add('btn-danger');
-    delBtn.innerText = 'x';
-    itemTitle.innerText = objPost.title;
-    itemBody.innerText = objPost.body;
-    list.appendChild(itemTitle);
-    list.appendChild(itemBody);
-    list.appendChild(delBtn);
-    posts.appendChild(list);
-    pTitle.value = '';
-    pBody.value = '';
-    $('.modal-backdrop').remove();
-    $('#exampleModal').modal('hide');
-    alert('post added');
-  } else {
-    alert('fill up data');
+  pre.push(e.key);
+  if (e.key == 'Escape') {
+    pre = [];
   }
-};
+  pre.splice(-code.length - 1, pre.length - code.length);
+  if (pre.join('').includes(code)) {
+    let anwser = window.confirm('top secret...');
+    if (anwser) {
+      let data = fetch('https://api.github.com/repos/elixir-lang/elixir/issues')
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          return json;
+        });
+      data.then((res) => {
+        res.slice(0, 5).map((r) => {
+          const item = document.createElement('li');
+          item.classList.add('list-group-item');
+          const itemTitle = document.createElement('h2');
+          const itemBody = document.createElement('p');
+          itemTitle.innerText = r.number;
+          itemBody.innerText = r.user.login;
+          item.appendChild(itemTitle);
+          item.appendChild(itemBody);
 
-window.addEventListener('load', () => {
-  setTimeout(genData, 1000);
+          dataObj.appendChild(item);
+          setTimeout(() => {
+            item.style.display = 'none';
+          }, 15000);
+        });
+      });
+    } else {
+      cornify_add();
+    }
+  }
 });
-
-const delPost = (e) => {
-  const li = e.target.parentElement;
-  li.parentNode.removeChild(li);
-};
-posts.addEventListener('click', delPost);
-
-let btn = document.querySelector('#add').addEventListener('click', addPost);
